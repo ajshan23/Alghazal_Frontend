eb0c46bfb062c747915c9bedeec2b8fedb157a22
ajshan23 <ajmalshahan23@gmail.com>
2025-09-10 03:19:46 +0530

    latest build after engineer edit

---
+++ build/assets/ProjectService-D2rruQrz.js
@@ -0,0 +1,1 @@
+import{ao as a}from"./index-BOaX7UE0.js";async function r(){return a.fetchData({url:"/project/dashboard",method:"get"})}export{r as a};
--- build/assets/ProjectService-DD-ei2mu.js
+++
@@ -1,1 +0,0 @@
-import{ao as a}from"./index-r9RqPD6B.js";async function r(){return a.fetchData({url:"/project/dashboard",method:"get"})}export{r as a};
